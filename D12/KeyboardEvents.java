0e30b3200c75b5137ece102f9fafe958157a4206
sumanta02 <sumanta87@protonmail.com>
2022-12-14 21:12:07 +0530

    simplified keyevent handling example

--- D12/KeyboardEvents.java
+++ D12/KeyboardEvents.java
@@ -3,64 +3,35 @@ import java.awt.event.*;
 import java.applet.*;
 
 public class KeyboardEvents extends Applet implements KeyListener {
-   
-   TextArea area, l;
+   Label l;
+   TextArea area;
 
    public KeyboardEvents() {
-      l = new TextArea();
-      l.setEditable(false);
-      l.setBounds(10, 350, 500, 500);
+      l = new Label();
+      l.setBounds(20, 50, 400, 20);
       area = new TextArea();
-      area.setBounds(10, 10, 300, 300);
+      area.setBounds(20, 80, 300, 300);
       area.addKeyListener(this);
       add(l);
       add(area);
-      setSize(900, 900);
+      setSize(400, 400);
       setLayout(null);
       setVisible(true);
    }
 
    public void keyPressed(KeyEvent e) {
-      displayInfo(e, "Key Pressed: ");
+         int keyCode = e.getKeyCode();
+         l.setText("Key Character: " + e.getKeyChar() + "    Key Pressed: " + e.getKeyText(keyCode));
    }
 
    public void keyReleased(KeyEvent e) {
-      displayInfo(e, "Key Released: ");
+         int keyCode = e.getKeyCode();
+         l.setText("Key Character: " + e.getKeyChar() + "    Key Pressed: " + e.getKeyText(keyCode));
    }
 
    public void keyTyped(KeyEvent e) {
-      displayInfo(e, "Key Typed: ");
-   }
-
-   void displayInfo(KeyEvent e, String output) {
-      int id = e.getID();
-        String keyString;
-        if (id == KeyEvent.KEY_TYPED) {
-            char c = e.getKeyChar();
-            keyString = "key character = '" + c + "'";
-        } else {
-            int keyCode = e.getKeyCode();
-            keyString = "key code = " + keyCode
-                    + " ("
-                    + KeyEvent.getKeyText(keyCode)
-                    + ")";
-        }
-      int modifiersEx = e.getModifiersEx();
-      String modString = "extended modifiers = " + modifiersEx;
-      String tmpString = KeyEvent.getModifiersExText(modifiersEx);
-        if (tmpString.length() > 0) {
-            modString += " (" + tmpString + ")";
-        } else {
-            modString += " (no extended modifiers)";
-        }
-        
-        String actionString = "action key? ";
-        if (e.isActionKey()) {
-            actionString += "YES";
-        } else {
-            actionString += "NO";
-        }
-        l.setText(output + "\n" + keyString + "\n" + modString + "\n" + actionString + "\n");
+         int keyCode = e.getKeyCode();
+         l.setText("Key Character: " + e.getKeyChar() + "    Key Pressed: " + e.getKeyText(keyCode));
    }
 
    public void init() {
@@ -69,7 +40,7 @@ public class KeyboardEvents extends Applet implements KeyListener {
 }
 /*
  * 
- * <applet code = KeyboardEvents.class width=900 height=900>
+ * <applet code = KeyboardEvents.class width=400 height=400>
  * 
  * </applet>
  * 
